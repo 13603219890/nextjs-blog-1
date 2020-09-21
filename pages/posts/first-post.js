a20281e8b5a119d7a30798499cad89c3f4be32ed
haocongmin <1095966795@qq.com>
2020-09-21 16:45:47 +0800

    Next渲染页面

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function X(){
+    return(
+        <div>First Post</div>
+    )
+}
+//渲染页面：
+//yarn add --dev @types/react @types/react-dom
+//import React from "react";
+//推测路径：/posts/first-post
+//总结：
+//1.约定大于配置
+//2.不需要配置，只需要约定，开发者自然意会
+//3.这是Rails的理念
